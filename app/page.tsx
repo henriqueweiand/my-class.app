80bd49d95d831caf18790555cf5943c15d98a240
henrique weiand <henriqueweiand@gmail.com>
2023-05-20 14:44:17 -0300

    added daisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <><h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1></>
+    <div>
+      <button className="btn">Hello daisyUI</button>
+    </div>
   )
 }
