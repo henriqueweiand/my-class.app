f4cb5d8f774de93bc3d9adb121a61b15b023e90d
henrique weiand <henriqueweiand@gmail.com>
2023-05-21 17:31:35 -0300

    added menus according user login

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,10 +2,13 @@
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import { SafeUser } from "@/app/types";
 
-interface NavbarProps { }
+interface NavbarProps {
+    currentUser?: SafeUser | null;
+}
 
-const Navbar: React.FC<NavbarProps> = ({ }) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
     const loginModal = useLoginModal();
     const registerModal = useRegisterModal();
 
@@ -15,10 +18,21 @@ const Navbar: React.FC<NavbarProps> = ({ }) => {
                 <a className="btn btn-ghost normal-case text-xl">My class</a>
             </div>
 
-            <div className="navbar-end">
-                <label className="btn mr-2" onClick={registerModal.onOpen}>Sign up</label>
-                <label className="btn" onClick={loginModal.onOpen}>Sign in</label>
-            </div>
+            {
+                currentUser ? (
+                    <div className="navbar-end">
+                        <label className="btn mr-2" onClick={registerModal.onOpen}>Create room</label>
+                        <label className="btn" onClick={loginModal.onOpen}>Log off</label>
+                    </div>
+                ) : (
+                    <div className="navbar-end">
+                        <label className="btn mr-2" onClick={registerModal.onOpen}>Sign up</label>
+                        <label className="btn" onClick={loginModal.onOpen}>Sign in</label>
+                    </div>
+                )
+            }
+
+
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ClientOnly from './components/ClientOnly';
 import Navbar from './components/navbar/Navbar';
 import ToasterProvider from './providers/ToasterProvider';
 import ModalsProvider from './providers/ModalsProvider';
+import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
   title: 'my-class.app',
@@ -15,18 +16,20 @@ const font = Nunito({
   subsets: ["latin"]
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en" data-theme="dark">
       <body className={font.className} suppressHydrationWarning={true}>
         <ModalsProvider />
         <ClientOnly>
           <ToasterProvider />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
 
         <div>
